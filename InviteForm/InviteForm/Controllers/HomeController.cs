8c1aec2c78c33c0adc609f0d449816f2b730b377
ranolios <65423071+ranolioss@users.noreply.github.com>
2022-05-19 17:01:54 +0200

    InviteFrom with c#

---
+++ InviteForm/InviteForm/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using InviteForm.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InviteForm.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good morning" : "Good Evening";
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ViewResult EnrollNow()
+        {
+            return View();
+            //return View("Thanks" , response);  
+        }
+        [HttpPost]
+        public ViewResult EnrollNow(Response response)
+       
+            {
+                if (ModelState.IsValid)
+                {
+
+                    return View("Thanks", response);
+                }
+                else
+                    return View();
+            }
+        }
+}
---
+++ InviteForm/InviteForm/Models/Response.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace InviteForm.Models
+{
+    public class Response
+    {
+        [Required(ErrorMessage = "Please enter your name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter your number")]
+        public string MobileNumber { get; set; }
+
+        [Required(ErrorMessage = "Please enter your email ID")]
+        [RegularExpression(".+\\@.+\\..+",
+        ErrorMessage = "Please neter your address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "will you come to our party? ")]
+        public bool? AttendOrNot { get; set; }
+    }
+}
